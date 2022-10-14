89136efdc5bfd2ad7c657a1b45c9101bea38537d
stampmtestnet <stampmtestnet.v1@gmail.com>
2022-10-14 16:25:49 +0700

    first commit

---
+++ components/component.jsx
@@ -0,0 +1,14 @@
+import Hero from './hero/hero';
+import Bids from './bids/bids';
+import Top_collection from './collectrions/top_collection';
+import Tranding_category from './categories/tranding_categories';
+import NewseLatter from './blog/newseLatter';
+import HeadLine from './headLine'
+export {
+	Hero,
+	Bids,
+	Top_collection,
+	Tranding_category,
+	NewseLatter,
+    HeadLine
+} 
